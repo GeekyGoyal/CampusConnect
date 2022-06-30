0b49a279b72701615277d5977a51b31e03f8b73f
Aparna Goyal <aparnagoyal.0003@gmail.com>
2022-06-30 16:04:56 +0530

    updated

---
+++ client/build/precache-manifest.c9d693642940fb48223f8001d6608b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba44d293e34fecf2eace294b818d620f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54c06497881f593395fa",
+    "url": "/static/css/main.cd7cf254.chunk.css"
+  },
+  {
+    "revision": "e85ea8eb31a030786073",
+    "url": "/static/js/2.78850093.chunk.js"
+  },
+  {
+    "revision": "54c06497881f593395fa",
+    "url": "/static/js/main.53289c96.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2305b7ff223d0929ae74ad4855ab5bc",
+    "url": "/static/media/pic.e2305b7f.png"
+  }
+]);
