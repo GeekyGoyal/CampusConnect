2d66ecfdc0dfcede0c9fc25c9ef6e0148406019b
Aparna Goyal <aparnagoyal.0003@gmail.com>
2022-09-12 05:40:12 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -30,8 +30,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
@@ -39,8 +37,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve Static Assets in production
-if(process.env.NODE_ENV === 'production')
-{
+if(process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
@@ -48,6 +45,7 @@ if(process.env.NODE_ENV === 'production')
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     })
 }
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
